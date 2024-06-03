58c9ed7aad9dd25989cab1fff7ef24f15563be74
ethancoote <realethancoote@gmail.com>
2024-06-03 04:35:53 -0400

    data collected, saved in new file

--- elo_generator/main.py
+++ elo_generator/main.py
@@ -5,10 +5,10 @@ import os
 current_path = os.path.dirname(os.path.abspath(__file__))
 
 def run_init():
-    results = stat_scraper.init_dataset()
-    # results = stat_scraper.get_event_results("http://www.ufcstats.com/event-details/8fa2b06572365321")
+    # results = stat_scraper.init_dataset()
+    results = stat_scraper.get_event_results("http://www.ufcstats.com/event-details/6420efac0578988b")
 
-    f = open(current_path + "/fighter_data/fighter.data.txt", "w")
+    f = open(current_path + "/fighter_data/fighter_data.txt", "w")
     for fight in results:
         f.write(f"{fight[0]}-{fight[1]}-{fight[2]}-{fight[3]}-{fight[4]}\n")
 
@@ -16,4 +16,5 @@ def run_elo_sim():
     print("not active")
 
 run_init()
+#run_elo_sim()
 
