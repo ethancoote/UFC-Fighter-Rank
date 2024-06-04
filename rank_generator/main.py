2aec218e9ce4dc5fc9da6313ff85edfdad39ba2d
ethancoote <realethancoote@gmail.com>
2024-06-04 03:42:17 -0400

    now gets all fights and past stats into rank sim

--- rank_generator/main.py
+++ rank_generator/main.py
@@ -14,7 +14,7 @@ def run_init():
         f.write(f"{fight[0]}-{fight[1]}-{fight[2]}-{fight[3]}-{fight[4]}\n")
 
 def run_rank_sim():
-    rank_sim.rank_sim(current_path + "/fighter_data/fighter_data_test_sample.txt")
+    rank_sim.rank_sim(current_path + "/fighter_data/fighter_data_test_sample.txt", current_path + "/rank_data/fighter_rank.txt")
     
 
 # run_init()
--- rank_generator/rank_sim.py
+++ rank_generator/rank_sim.py
@@ -1,11 +1,11 @@
 # Takes all UFC fights in order and give each fighter an elo rating
-def rank_sim(filename):
-    all_fights = get_fights_data(filename)
-    print(all_fights)
-
+def rank_sim(fight_list_filename, fighter_rank_filename):
+    all_fights = get_fights_data(fight_list_filename)
+    all_ranks = get_ranks_data(fighter_rank_filename)
+    # update_ranks(all_fights, all_ranks)
 
 def get_fights_data(filename):
-    all_fights = []
+    all_data = []
     try:
         f = open(filename, "r")
     except:
@@ -13,7 +13,30 @@ def get_fights_data(filename):
         return([])
     
     for line in f:
-        fight = line.split("-")
-        all_fights.append(fight)
+        line = line.strip()
+        split_line = line.split("-")
+        all_data.append(split_line)
 
-    return all_fights
+    return all_data
+
+def get_ranks_data(filename):
+    all_data = {}
+    try:
+        f = open(filename, "r")
+    except:
+        print("File not found...")
+        return({})
+    
+    for line in f:
+        line = line.strip()
+        split_line = line.split("-")
+        name = split_line[0]
+        split_line.pop(0)
+        all_data[name] = split_line
+
+    return all_data
+
+
+
+#def update_ranks(all_fights, all_ranks):
+#    for fight in all_fights:
