cb9c4d6f935fcf7871dc8fffb63e062e11b8c244
ethancoote <realethancoote@gmail.com>
2024-06-09 22:20:47 -0400

    adding gitignore and package.json files

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("test");
+fetch(path.__dirname() + "/rank_generator/rank_data/ordered_list.txt")
+    .then((res) => res.text())
+    .then((text) => {
+        console.log(text)
+    })
+    .catch((e) => console.error(e))
