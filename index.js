8fdfe78fdb1da09bd28d9c99176fdd0849e57869
ethancoote <realethancoote@gmail.com>
2024-06-22 01:19:48 -0400

    adding firebase and front end display

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-fetch(path.__dirname() + "/rank_generator/rank_data/ordered_list.txt")
-    .then((res) => res.text())
-    .then((text) => {
-        console.log(text)
-    })
-    .catch((e) => console.error(e))
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDnYM6AbGp27J-u2r5nLL9Ie0oZTmsfrqk",
+    authDomain: "mma-stats-38214.firebaseapp.com",
+    projectId: "mma-stats-38214",
+    storageBucket: "mma-stats-38214.appspot.com",
+    messagingSenderId: "73526909394",
+    appId: "1:73526909394:web:5a14feb4e6eedb2dac900b",
+    measurementId: "G-C29SR87P52"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
