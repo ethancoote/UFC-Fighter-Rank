114c998ba0c7555a4d4ca560ac919e8416368443
ethancoote <realethancoote@gmail.com>
2024-06-22 01:40:32 -0400

    adding compatability for rankings.json

--- public/index.js
+++ public/index.js
@@ -20,3 +20,40 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+PetiteVue.createApp({
+    search: '',
+    
+    onChange() {
+        update_list(this.search);
+    }
+    
+}).mount("#body");
+
+let data = [];
+fetch("../rank_generator/rank_data/rankings.json")
+.then(function(res){
+    return res.json();
+})
+.then(function(products){
+    data = products;
+    update_list('');
+})
+    
+function update_list(filter_string) {
+    let placeholder = document.querySelector("#table-body-0");
+    let out = "";
+    let filtered_data = data;
+    for(let product of filtered_data){
+        if (product.name.toLowerCase().includes(filter_string.toLowerCase()) || filter_string == '') {
+            out += `
+            <tr>
+                <td>${product.rank}</td>
+                <td>${product.name}</td>
+                <td>${product.rating}</td>
+            </tr>
+            `;
+        }
+    }
+    placeholder.innerHTML = out;
+}
